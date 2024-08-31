f20a5c4ffd7fd14552fee0694b9137062b9a5f94
llOrTeGall <IvanOrtega_97@hotmail.com>
2024-08-31 16:52:25 +0000

    tables creadas

--- src/utils/scripts.ts
+++ src/utils/scripts.ts
@@ -2,11 +2,17 @@ import { DbConfig, Sucursal, Vendedor } from './types'
 import mysql from 'mysql2/promise'
 import 'dotenv/config'
 
-const HOST = process.env.DB_POWERBI_HOST!
-const USER = process.env.DB_POWERBI_USER!
-const PORT = parseInt(process.env.DB_POWERBI_PORT!)
-const PASSWORD = process.env.DB_POWERBI_PASS!
-const DATABASE = process.env.DB_POWERBI_DB!
+// const HOST = process.env.DB_POWERBI_HOST!
+// const USER = process.env.DB_POWERBI_USER!
+// const PORT = parseInt(process.env.DB_POWERBI_PORT!)
+// const PASSWORD = process.env.DB_POWERBI_PASS!
+// const DATABASE = process.env.DB_POWERBI_DB!
+
+const HOST = process.env.DB_COMPANY_HOST!
+const USER = process.env.DB_COMPANY_USER!
+const PORT = parseInt(process.env.DB_COMPANY_PORT!)
+const PASSWORD = process.env.DB_COMPANY_PASS!
+const DATABASE = process.env.DB_COMPANY_DB!
 
 // Configuración de la conexión
 const dbConfig: DbConfig = {
@@ -40,3 +46,42 @@ export const getSucPoweBi = async (ccosto: number): Promise<Sucursal[]> => {
   }
 }
 
+export const createTable = async (codigo: number) => {
+  // Validar el nombre de la tabla para evitar inyecciones SQL
+  if (!Number.isInteger(codigo) || codigo <= 0) {
+    throw new Error('Código de tabla inválido')
+  }
+
+  const tableName = `table_${codigo}` // Prefijo para evitar nombres de tablas no válidos
+
+  const query = `
+    CREATE TABLE IF NOT EXISTS ${tableName} (
+      CHANCE INT,
+      PAGAMAS INT,
+      PAGATODO INT,
+      PAGATODO_JAMUNDI INT,
+      CHOLADITO INT,
+      PATA_MILLONARIA INT,
+      DOBLECHANCE INT,
+      CHANCE_MILLONARIO INT,
+      ASTRO INT,
+      LOTERIA_FISICA INT,
+      LOTERIA_VIRTUAL INT,
+      BETPLAY INT,
+      GIROS INT,
+      SOAT INT,
+      RECAUDOS INT,
+      RECARGAS INT,
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    );
+  `
+
+  try {
+    const [rows] = await connection.query(query)
+    return rows
+  } catch (error) {
+    console.error(error)
+    throw new Error('Error al crear la tabla')
+  }
+}
