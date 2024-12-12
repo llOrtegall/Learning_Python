b1d7e44ac17e7509927fc36cb89775b703283916
llOrtegall <IvanOrtega_97@hotmail.com>
2024-12-12 16:07:25 -0500

    feat: add withdraw and deactivate methods to BankAccount class; create Book class in libraryClass.py

--- 1.CoursePlatzi/bankAccount.py
+++ 1.CoursePlatzi/bankAccount.py
@@ -3,13 +3,37 @@ class BankAccount:
         self.accoun_holder = accoun_holder
         self.balance = balance
         self.is_active = True
-    
-    def deposit (self, amount):
+
+    def deposit(self, amount):
         if self.is_active:
             self.balance += amount
-            print(f'${amount} has been deposited to your account. Your new balance is ${self.balance}')
+            print(
+                f"${amount} has been deposited to your account. Your new balance is ${self.balance}"
+            )
         else:
-            return 'Account is closed'
+            return "Account is closed"
+
+    def withdraw(self, amount):
+        if self.is_active:
+            if amount <= self.balance:
+                self.balance -= amount
+                print(
+                    f"Service withdraw for: ${amount}. Your new balance is ${self.balance}"
+                )
+            else:
+                print("Insufficient funds")
+
+    def deactivate(self):
+        self.is_active = False
+        print("Account is closed")
 
 
-        
+acount1 = BankAccount("Juan", 1000)
+acount1.deposit(200)
+acount1.withdraw(500)
+acount1.deactivate()
+print("====================================================================")
+acount2 = BankAccount("Pedro", 500)
+acount2.deposit(100)
+acount2.withdraw(200)
+acount2.deactivate()
